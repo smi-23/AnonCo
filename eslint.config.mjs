fa3fb466da9dd3bac5c017a91e3bfdeae6c5fb35
smi-23 <latack789@naver.com>
2025-02-15 23:54:54 +0900

    Chore: eslint, next.js 관련 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      // 사용하지 않는 임포트 등에 관한 에러 처리
+      // "no-unused-vars": "off",
+      // "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+]
 
 export default eslintConfig;
