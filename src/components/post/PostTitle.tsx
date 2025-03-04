6c14b01404c6ae1cb3c4108e1e91bc099fd1a48e
smi-23 <latack789@naver.com>
2025-03-05 00:34:24 +0900

    Chore: title에서 props만 사용하던 것에서 API요청을 통해 값을 받아오는 것으로 변경

--- src/components/post/PostTitle.tsx
+++ src/components/post/PostTitle.tsx
@@ -1,13 +1,33 @@
+'use client'
 import { Box, Divider, Typography } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 interface PostTitleProps {
   category: string
 }
 
 export default function PostTitle({ category }: PostTitleProps) {
+  const [categoryTitle, setCategoryTitle] = useState<string | ''>('')
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/category/${category}`)
+        setCategoryTitle(response.data.data.title)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchCategory()
+  }, [category])
+
   return (
     <Box>
-      <Typography variant="h4">{category}</Typography>
+      <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+        {categoryTitle}
+      </Typography>
       <Divider sx={{ my: 3 }} />
     </Box>
   )
